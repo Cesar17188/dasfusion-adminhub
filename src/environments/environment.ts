47532e7ebb9839f7076a31c53a1408e00b4e7c00
Cesar <cesareli17188@gmail.com>
2026-09-13 20:32:08 -0500

    integracion de las variables de ambiente para conexión de la base de datos de supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://whlxncobakktxghxdyfw.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndobHhuY29iYWtrdHhnaHhkeWZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0NzAxOTIsImV4cCI6MjEwNDA0NjE5Mn0.kxK1rimna6s4qYXpHICqi3gPB4St7yXe9eoeK_t5sXk',
+};
